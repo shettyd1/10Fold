66a99af85617a780da8fa657cb3602725ef88618
Divya Shetty <sdiana220695@gmail.com>
2024-06-27 13:52:49 -0400

    React

---
+++ React.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const myFirstElement = <h1> Hello!<h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myFirstElement);
+
+function button() {
+    return (
+         <button> </button>
+            
+            );
+}
+
+export default function MyAccount() {
+    return (
+            <div>
+            <h1> Welcome </h1>
+            <button />
+            </div>
+            
+            );
+    
+}
+
+function AboutPage() {
+    return(
+           <>
+           <h1> About You </h1>
+           <p> Hello there.<br /> Your list of videos </p>
+           </>
+           );
+    
+}
+
+<img className="avatar" />
+.avatar {
+    border-radius: 50%;
+}
+
+
+return (
+        <h1>
+        
+            <h1>
+            {user.name}
+            </h1>
+            
+        
+        
+        
+        );
