3b020ffea46997fd00c85450219ff3efbe7a456e
Divya Shetty <sdiana220695@gmail.com>
2024-11-20 01:46:34 +0530

    server

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+import {useState, useEffect } from 'react';
+import {fetchBio} from './api.js'
+
+export default function Page() {
+    const [person, setPerson] = useState('Alice');
+    const [bio, setBio] = useState(null);
+    
+    useEffect(() => {
+        setBio(null);
+        fetchBio(person).then(result => {
+            setBio(result);
+        });
+    }, [person]);
+    
+    return (
+            <>
+            <selct value={person} onChange={e => {
+                setPerson(e.target.value);
+            }}>
+            <option value="Alice">Alice</option>
+            <option value="Bob">Bob</option>
+            <option value="Taylor">Taylor</option>
+        </select>
+        <hr />
+            <p><i>{bio ?? 'Loading...'}</i></p>
+            </>
+            );
+}
+
+
 <!DOCTYPE html>
 <html>
 </html>
--- video.js
+++ video.js
@@ -20,7 +20,56 @@ function VideoPlayer({src, isPlaying}) {
     
     const ref = useRef(null);
     const [count, setCount] = useState(0);
+    
+    function Playground() {
+        const [text, setText] = useState('a');
+    }
     useEffect(() => {
+        logVisit(url);
+        if (typeof window !== 'undefined') {
+            checkAuthToken();
+        }
+        function onTimeout() {
+            console.log('Clock' + text);
+            clearTimeout(timeoutId);
+        };
+        
+        console.log('Schedule "' + text + '"log"');
+        const timeoutId = setTimeout(onTimeout, 3000);
+        
+        return () => {
+            console.log('Cancel "' + text + '"log');
+            clearTimeout(timeoutId);
+        };
+        
+        return (
+          <>
+            <label>
+                What to log:{''}
+                <input
+                value={text}
+                onChange={e => setText(e.target.value)}
+            />
+          </label>
+        <h1>{text}</h1>
+       </>
+    );
+        function App() {
+            
+        }
+        fetch('api/buy', {method: 'POST'});
+        function handleScroll(e) {
+            console.log(window.scrollX, window.scrollY)
+            
+        }
+        window.addEventListener('scroll', handleScroll);
+        
+        const node  = ref.current;
+        node.style.opacity = 1;
+        return () => {
+            node.style.opacity = 0;
+        };
+        
         if (!connectionRef.current){
             const connection = createConnection();
             connection.connect();
@@ -34,7 +83,28 @@ function VideoPlayer({src, isPlaying}) {
             console.log('Calling video.pause');
             ref.current.pause();
         }
-    }, [], [zoomLevel], [isPlaying]);
+        let ignore = false;
+        
+        function TodoList() {
+            const todos = useSomeDataLibrary('/api/user/${userId}/todos');
+        }
+        
+        async function startFetching() {
+            const json = await fetchTodos(userId);
+            if (!ignore) {
+                setTodos(json);
+            }
+            
+        }
+        
+        startFetching();
+        
+        return () => {
+            ignore = true;
+        };
+        
+        
+    }, [url], [], [text], [zoomLevel], [isPlaying], [userId]);
     
     return <video ref={ref} src{src} loop playsInline />;
     
